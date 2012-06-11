42028f3a650ac4de0317978451b63e2bf6ce28c5
Thiago Lagden <lagden@gmail.com>
2012-06-11 18:27:14 -0300

    fixtures e schemas created

---
+++ apps/frontend/modules/produto/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+* produto actions.
+*
+* @package    sfI18nSample
+* @subpackage produto
+* @author     Thiago Lagden
+* @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+*/
+class produtoActions extends sfActions
+{
+    public function executeIndex(sfWebRequest $request)
+    {
+
+    }
+
+    public function executeShow(sfWebRequest $request)
+    {
+
+    }
+}
---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    sfI18nSample
+ * @subpackage form
+ * @author     Thiago Lagden 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
