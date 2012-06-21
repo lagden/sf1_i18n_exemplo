46b438064f740b14854dfe87f216fc2fb6147226
Thiago Lagden <lagden@gmail.com>
2012-06-20 23:16:13 -0300

    added fill produtos

--- apps/frontend/modules/produto/actions/actions.class.php
+++ apps/frontend/modules/produto/actions/actions.class.php
@@ -26,8 +26,33 @@ class produtoActions extends sfActions
         
         $r = ($q = $request->getParameter('q',false)) ? SearchLucene::getLuceneQuery($q, $tbl, $culture) : false;
         $r = ($r) ? $tbl->findByCulture($culture, $r)->execute()->toArray() : ['none'];
-        
+
         $this->getResponse()->setContentType('application/json');
         return $this->renderText(json_encode($r));
+    }
+
+    public function executeFixtures(sfWebRequest $request)
+    {
+        // Fill products
+        for ($xxx = 1; $xxx < 10; $xxx++)
+        {
+            $hash = md5(time() . mt_rand());
+            $prod = new Product();
+
+            $prod->category_id = 1;
+            $prod->route = "DVD do Homem Aranha | {$hash}";
+            $prod->is_active = 1;
+
+            $prod->Translation['pt']->name = "DVD do Homem Aranha {$xxx} | {$hash}";
+            $prod->Translation['pt']->description  = "Essa é uma descrição em pt {$xxx} | {$hash}";
+            $prod->Translation['pt']->price  = 29.23 + $xxx;
+
+            $prod->Translation['en']->name = "Spider-man DVD {$xxx} | {$hash}";
+            $prod->Translation['en']->description  = "This is a description in en {$xxx} | {$hash}";
+            $prod->Translation['en']->price  = 10.23 + $xxx;
+            $prod->save();
+        }
+        $this->getResponse()->setContentType('text/plain');
+        return $this->renderText('filled products');
     }
 }
